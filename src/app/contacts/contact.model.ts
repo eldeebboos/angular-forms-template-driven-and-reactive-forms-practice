1bbdb2a8db9e0d9f6fdc0a57d2aafc2e3b2fdfc0
eldeebboos <eldeeb_boos@yahoo.com>
2025-12-23 21:24:08 +0200

    add initial template driven forms

---
+++ src/app/contacts/contact.model.ts
@@ -0,0 +1,36 @@
+export interface Contact {
+  id: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  favoritesRanking: number | null;
+  personal: boolean;
+  notes: string;
+  phone: Phone;
+  address: Address;
+}
+
+export interface Phone {
+  phoneNumber: string;
+  phoneType: string;
+}
+
+export interface Address {
+  streetAddress: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  addressType: string;
+}
+
+export const phoneTypes = [
+  { value: 'mobile', title: 'Mobile' },
+  { value: 'work', title: 'Work' },
+  { value: 'other', title: 'Other' },
+];
+
+export const addressTypes = [
+  { value: 'home', title: 'Home' },
+  { value: 'work', title: 'Work' },
+  { value: 'other', title: 'Other' },
+];
---
+++ src/app/edit-contact/edit-contact.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { addressTypes, Contact, phoneTypes } from '../contacts/contact.model';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ContactsService } from '../contacts/contacts.service';
+
+@Component({
+  imports: [CommonModule, FormsModule],
+  templateUrl: './edit-contact.component.html',
+  styleUrls: ['./edit-contact.component.css'],
+})
+export class EditContactComponent implements OnInit {
+  phoneTypes = phoneTypes;
+  addressTypes = addressTypes;
+  contact: Contact = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    favoritesRanking: 0,
+    personal: false,
+    notes: '',
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  };
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contactService: ContactsService
+  ) {}
+
+  ngOnInit() {
+    const contactId = this.route.snapshot.params['id'];
+    if (!contactId) return;
+
+    this.contactService.getContact(contactId).subscribe((contact) => {
+      if (contact) this.contact = contact;
+    });
+  }
+
+  saveContact(form: NgForm) {
+    console.log(form.value);
+
+    this.contactService.saveContact(this.contact).subscribe({
+      next: () => this.router.navigate(['/contacts']),
+    });
+  }
+}
